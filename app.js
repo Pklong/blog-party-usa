cc5e93af42c8c3998dc1baa340c3546be7a03c25
Patrick Kovach-Long <PatrickKLong@gmail.com>
2017-09-11 17:38:13 -0400

    Add body-parser middleware

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const routes = require('./routes/index')
 
 app.set('views', './views')
 app.set('view engine', 'pug')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/', routes.blogRouter)
 
