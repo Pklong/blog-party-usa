a7a1239e63e17a52ae15040d72428f6dad85b27a
Patrick Kovach-Long <PatrickKLong@gmail.com>
2017-09-13 15:28:27 -0400

    Alter mongo to use hosted db

--- start.js
+++ start.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/blog-party-dev', {
-  useMongoClient: true,
-  promiseLibrary: require('bluebird')
+require('dotenv').config({ path: 'variables.env' })
+mongoose.connect(process.env.DATABASE, {
+  useMongoClient: true
 })
-const db = mongoose.connection
 
 const app = require('./app')
 
-db.on('error', console.error.bind(console, 'connection error:'))
+mongoose.connection.on(
+  'error',
+  console.error.bind(console, 'connection error:')
+)
 
-db.once('open', function() {
-  app.listen(3000, function() {
-    console.log('listening on 3000')
-  })
+app.listen(3000, function() {
+  console.log('listening on 3000')
 })
