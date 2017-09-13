82fc9fb64f316dc16106a1250a0896ce80377d2a
Patrick Kovach-Long <PatrickKLong@gmail.com>
2017-09-13 15:27:46 -0400

    Add static file serving

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
@@ -8,7 +10,7 @@ app.set('view engine', 'pug')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', routes.blogRouter)
 
 module.exports = app
